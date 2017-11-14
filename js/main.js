2cbbdb16d93df55ecbe11a63057c505a2dd7ac9e
CS 171 Final Project <ckardish12@gmail.com>
2017-11-13 23:16:54 -0500

    Crime rates craph proto v1

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
   $('#fullpage').fullpage();
 
   queue()
-  .defer(d3.csv, 'data/drug-seizures.csv')
-  .await(createVisualization);
+      .defer(d3.csv, 'data/drug-seizures.csv')
+      .defer(d3.csv, 'data/crime rates/men and women all ages.csv')
+      .await(createVisualization);
 });
 
 /**
  * Render visualizations
  */
-function createVisualization(error, drugSeizuresData) {
+function createVisualization(error, drugSeizuresData, crimeRatesData) {
   seizuresChart = new SeizuresChart('drug-seizures-chart', drugSeizuresData);
+  crimesChart = new CrimeRateChart('crime-rates-chart', crimeRatesData);
+
 }
 
 // Variables for the visualization instances
