8c2be9eec448f2c94d457e96c45f57524145f373
CS 171 Final Project <ckardish12@gmail.com>
2017-11-19 15:34:08 -0500

    Lab 10 Sorted update

--- Lab 10/js/main.js
+++ Lab 10/js/main.js
@@ -1,4 +1,4 @@
-var marriges =
+var marriages =
     [[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
     [0,0,0,0,0,1,1,0,1,0,0,0,0,0,0,0],
     [0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
@@ -44,20 +44,22 @@ function getSum(total, num) {
 // combine all the data
 d3.csv("data/florentine-familiy-attributes.csv", function(error, csvData){
     if(!error){
-        var data = csvData;
+        data = csvData;
 
         // convert variables that should be numeric to numeric
         // add the marrige and business ties
         for (var i = 0; i < data.length; i++) {
             data[i].index = i;
-            data[i].allRelations = business[i].reduce(getSum) + marriges[i].reduce(getSum);
+            data[i].allRelations = business[i].reduce(getSum) + marriages[i].reduce(getSum);
             data[i].businessTies = business[i].reduce(getSum);
             data[i].businessValues = business[i];
-            data[i].marriges = marriges[i].reduce(getSum);
-            data[i].marrigeValues = marriges[i];
-            data[i].Priorates = +data[i].Priorates;
+            data[i].marriages = marriages[i].reduce(getSum);
+            data[i].marrigeValues = marriages[i];
             data[i].Wealth = +data[i].Wealth;
-
+            data[i].Priorates = +data[i].Priorates;
+            if(isNaN(data[i].Priorates)){
+                data[i].Priorates = 0;
+            }
         }
 
         // Declare number of rows and columns
@@ -81,13 +83,13 @@ d3.csv("data/florentine-familiy-attributes.csv", function(error, csvData){
         //     .attr("height", height);
 
         // From http://bl.ocks.org/srosenthal/2770072
-        var margin = {top: 0, right: 0, bottom: 0, left: 0},
+        margin = {top: 0, right: 0, bottom: 0, left: 0},
             width = 800,
             height = 800;
 
-        var cellHeight = 21, cellWidth = 21, cellPadding = 10;
+        cellHeight = 21, cellWidth = 21, cellPadding = 10;
 
-        var svg = d3.select("body").append("svg")
+        svg = d3.select("#matrix").append("svg")
             .attr("width", width + margin.left + margin.right - 100)
             .attr("height", height + margin.top + margin.bottom - 100)
             .style("margin-left", -margin.left + "px")
@@ -100,25 +102,20 @@ d3.csv("data/florentine-familiy-attributes.csv", function(error, csvData){
             .attr("width", width)
             .attr("height", height);
 
-        var x = d3.scaleBand()
+        x = d3.scaleBand()
             .domain(d3.range(numcols))
             .rangeRound([0, width - 300]);
 
-        var y = d3.scaleBand()
+        y = d3.scaleBand()
             .domain(d3.range(numrows))
             .rangeRound([0, height - 300]);
 
-        var colorMap = d3.scaleLinear()
+        colorMap = d3.scaleLinear()
             .domain([0, 1])
             .range(["lightgray", "purple"]);
         //.range(["red", "black", "green"]);
         //.range(["brown", "#ddd", "darkgreen"]);
 
-        var row = svg.selectAll(".row")
-            .data(business)
-            .enter().append("g")
-            .attr("class", "row")
-            .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
 
         // row.selectAll(".cell")
         //     .data(function(d) { return d; })
@@ -131,78 +128,6 @@ d3.csv("data/florentine-familiy-attributes.csv", function(error, csvData){
         //
         // row.append("line")
         //     .attr("x2", width);
-
-        row.append("text")
-            .data(data)
-            .attr("x", 0)
-            .attr("y", y.bandwidth() / 2)
-            .attr("dy", ".32em")
-            .attr("text-anchor", "end")
-            .text(function(d, i) { return d.Family; })
-            .attr("transform", function(d, i) { return "translate(-5," + (-5) + ")"; });
-
-
-
-        var squarecounter = -1;
-        var columncounter = 0;
-
-        // D3's enter, update, exit pattern
-        // From lab
-        var trianglePathMarrige = row.selectAll(".triangle-path")
-            .data(data);
-
-        trianglePathMarrige.enter().append("path")
-            .attr("d", function(d, index) {
-                // Shift the triangles on the x-axis (columns)
-                var x = (cellWidth + cellPadding) * index;
-
-                // All triangles of the same row have the same y-coordinates
-                // Vertical shifting is already done by transforming the group elements
-                var y = 0;
-
-                return 'M ' + x +' '+ y + ' l ' + cellWidth + ' 0 l 0 ' + cellHeight + ' z';
-            })
-            .attr("fill", function(d, index){
-                squarecounter = squarecounter + 1;
-                columncounter = parseInt(squarecounter / 16);
-                if (d.marrigeValues[columncounter]===1){
-                    return "blue";
-                }
-                else{
-                    return "gray";
-                }
-            });
-
-
-        squarecounter = -1;
-        columncounter = 0;
-
-        var trianglePathBiz = row.selectAll(".triangle-path")
-            .data(data);
-
-        trianglePathBiz.enter().append("path")
-            .attr("d", function(d, index) {
-                // Shift the triangles on the x-axis (columns)
-                var x = (cellWidth + cellPadding) * index;
-
-                // All triangles of the same row have the same y-coordinates
-                // Vertical shifting is already done by transforming the group elements
-                var y = 0;
-
-                return 'M ' + x +' '+ y + ' l 0 ' + cellWidth + ' l ' + cellHeight + ' 0 z';
-            })
-            .attr("fill", function(d, index){
-                squarecounter = squarecounter + 1;
-                columncounter = parseInt(squarecounter / 16);
-                if (d.businessValues[columncounter]===1){
-                    return "orange";
-                }
-                else{
-                    return "gray";
-                }
-            })
-            .attr("stroke", "none");
-
 
 
         var column = svg.selectAll(".column")
@@ -223,9 +148,126 @@ d3.csv("data/florentine-familiy-attributes.csv", function(error, csvData){
             .attr("text-anchor", "start")
             .text(function(d, i) { return d.Family; });
 
-        row.selectAll(".cell")
-            .data(function(d, i) { return business[i]; })
-            .style("fill", colorMap);
+        updateVis();
 
     }
 });
+
+function updateVis(){
+
+    displayData = data;
+
+    var selected = document.getElementById("sortable").value;
+
+    switch (selected){
+        case "businesses":
+            displayData.sort(function(a,b){
+                 return b.businessTies - a.businessTies;
+            });
+            break;
+        case "marriages":
+            displayData.sort(function(a,b){
+                return b.marriages - a.marriages;
+            });
+            break;
+        case "relationships":
+            displayData.sort(function(a,b){
+                return b.allRelations - a.allRelations;
+            });
+            break;
+        case "wealth":
+            displayData.sort(function(a,b){
+                return b.Wealth - a.Wealth;
+            });
+            break;
+        case "seats":
+            displayData.sort(function(a,b){
+                return b.Priorates - a.Priorates;
+            });
+            break;
+    }
+
+    console.log(displayData);
+
+
+
+    var row = svg.selectAll(".row")
+        .data(displayData)
+        .enter().append("g")
+        .attr("class", "row")
+        .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
+
+    row.append("text")
+        .data(displayData)
+        .attr("x", 0)
+        .attr("y", y.bandwidth() / 2)
+        .attr("dy", ".32em")
+        .attr("text-anchor", "end")
+        .text(function(d, i) { return d.Family; })
+        .attr("transform", function(d, i) { return "translate(-5," + (-5) + ")"; });
+
+    row.selectAll(".cell")
+        .data(function(d, i) { return business[i]; })
+        .style("fill", colorMap);
+
+
+    var squarecounter = -1;
+    var columncounter = 0;
+
+    // D3's enter, update, exit pattern
+    // From lab
+    var trianglePathMarriage = row.selectAll(".triangle-path")
+        .data(displayData);
+
+    trianglePathMarriage.enter().append("path")
+        .attr("d", function(d, index) {
+            // Shift the triangles on the x-axis (columns)
+            var x = (cellWidth + cellPadding) * index;
+
+            // All triangles of the same row have the same y-coordinates
+            // Vertical shifting is already done by transforming the group elements
+            var y = 0;
+
+            return 'M ' + x +' '+ y + ' l ' + cellWidth + ' 0 l 0 ' + cellHeight + ' z';
+        })
+        .attr("fill", function(d, index){
+            squarecounter = squarecounter + 1;
+            columncounter = parseInt(squarecounter / 16);
+            if (d.marrigeValues[columncounter]===1){
+                return "blue";
+            }
+            else{
+                return "gray";
+            }
+        });
+
+
+    squarecounter = -1;
+    columncounter = 0;
+
+    var trianglePathBiz = row.selectAll(".triangle-path")
+        .data(displayData);
+
+    trianglePathBiz.enter().append("path")
+        .attr("d", function(d, index) {
+            // Shift the triangles on the x-axis (columns)
+            var x = (cellWidth + cellPadding) * index;
+
+            // All triangles of the same row have the same y-coordinates
+            // Vertical shifting is already done by transforming the group elements
+            var y = 0;
+
+            return 'M ' + x +' '+ y + ' l 0 ' + cellWidth + ' l ' + cellHeight + ' 0 z';
+        })
+        .attr("fill", function(d, index){
+            squarecounter = squarecounter + 1;
+            columncounter = parseInt(squarecounter / 16);
+            if (d.businessValues[columncounter]===1){
+                return "orange";
+            }
+            else{
+                return "gray";
+            }
+        })
+        .attr("stroke", "none");
+}
